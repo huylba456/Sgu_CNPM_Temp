01fae7f29e251ce3b80f2723c5bc4e77c1d5fbd4
huy <huylba456@gmail.com>
2025-10-15 16:25:08 +0700

    delete mobile

--- mobile/src/context/AuthContext.js
+++
@@ -1,5 +0,0 @@
-import { createContext, useContext } from 'react';
-
-export const AuthContext = createContext({ user: null, setUser: () => {} });
-
-export const useAuth = () => useContext(AuthContext);
--- mobile/src/utils/id.js
+++
@@ -1,1 +0,0 @@
-export const generateId = () => `id-${Date.now().toString(36)}-${Math.random().toString(36).slice(2, 8)}`;
